ffd93fdbbdefe2e5dc58615fd5a1578c12133d2f
innerlize <alan.dev@outlook.kr>
2024-12-02 19:40:13 -0300

    Create 'Prettier' config file

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  arrowParens: "always",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
