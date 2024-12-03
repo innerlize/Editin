6b747bad9b8c87a49aea2ed40de009c2e640a0b9
innerlize <alan.dev@outlook.kr>
2024-12-03 08:35:07 -0300

    Apply 'Prettier' formatting to initial files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { rules: { 'react/react-in-jsx-scope': 'off' } },
   { settings: { react: { version: 'detect' } } },
 ];
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,16 +3,16 @@
  * @type {import("prettier").Config}
  */
 export default {
-  arrowParens: "always",
+  arrowParens: 'always',
   bracketSpacing: true,
   bracketSameLine: true,
   jsxBracketSameLine: false,
   jsxSingleQuote: false,
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   singleQuote: true,
   semi: true,
   printWidth: 100,
   useTabs: false,
   tabWidth: 2,
-  trailingComma: "es5",
+  trailingComma: 'es5',
 };
